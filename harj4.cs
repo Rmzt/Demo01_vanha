2ff30d2710284471916e2d5b3ce1ee30818eab83
Teemu Remes <K2131@student.jamk.fi>
2016-01-19 19:15:01 +0200

    Tehty harj 3-9 & vaihtolajittelu pohja

---
+++ harj4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Demo1
+{
+    class harj4
+    {
+        static void Main(string[] args)
+        {
+            int ika = 0;
+    
+            Console.WriteLine("Anna ikäsi: ");
+
+            ika = int.Parse(Console.ReadLine());
+
+            if (ika < 18) Console.WriteLine("Alaikäinen");
+            else if (ika <= 65) Console.WriteLine("Aikuinen");
+            else Console.WriteLine("Seniori");
+
+
+
+
+
+            Console.WriteLine("\n\nAll done - exiting app");
+        }
+    }
+}
---
+++ harj9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Demo1
+{
+    class harj9
+    {
+        static void Main(string[] args)
+        {
+            int luku, summa = 0;
+
+            do
+            {
+                Console.WriteLine("Anna Luku > ");
+                luku = int.Parse(Console.ReadLine());
+                summa += luku;
+            } while (luku != 0);
+
+            Console.WriteLine("Lukujen summa on " + summa);
+
+
+
+
+
+            Console.WriteLine("\n\nAll done - exiting app");
+        }
+    }
+}
