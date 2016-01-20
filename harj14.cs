38d5bb5ffd12cc93dd653932ec2d586fbe80c8a0
Teemu Remes <teemu1.remes@hotmail.com>
2016-01-20 16:25:09 +0200

    committi

---
+++ harj14.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Demo1
+{
+    class harj14
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_TAULU = 6;
+            int[] taulu = new int[MAX_TAULU] { 0, 0, 0, 0, 0, 0 };
+            int i, j, apu = 0;
+
+            do
+            {
+                Console.Write("Anna arvosana > ");
+                apu = int.Parse(Console.ReadLine());
+
+                switch (apu) {
+                    case 0: taulu[0]++; break;
+                    case 1: taulu[1]++; break;
+                    case 2: taulu[2]++; break;
+                    case 3: taulu[3]++; break;
+                    case 4: taulu[4]++; break;
+                    case 5: taulu[5]++; break;
+                }
+
+            } while (apu >= 0);
+
+            Console.WriteLine("Arvosanat:");
+            for (j = 0; j < MAX_TAULU; j++)
+            {
+                Console.Write(j + ":");
+                for (i = 0; i < taulu[j]; i++) Console.Write("*");
+                Console.WriteLine();
+            }
+        
+
+
+            Console.WriteLine("\n\nAll done - exiting app");
+        }
+    }
+}
+
